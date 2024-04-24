3b1835071ebe9c52b753938efd6e6485502c122f
Cam <133520431+cammclain@users.noreply.github.com>
2024-04-24 05:30:16 -0500

    Add missing imports and fix borrow error in read_id_passwords function

--- ice_cracker/src/main.rs
+++ ice_cracker/src/main.rs
@@ -3,6 +3,16 @@ use polars::prelude::*;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::process;
+use std::fs::File;
+use std::process;
+use md5;
+use phpass;
+use bcrypt;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::process;
+use std::io::{BufRead, BufReader};
+use std::process;
 
 fn main() -> PolarsResult<()> {
     let matches = App::new("Ice Cracker")
@@ -44,14 +54,11 @@ fn main() -> PolarsResult<()> {
     Ok(())
 }
 
-
 fn read_id_passwords(path: &str) -> PolarsResult<DataFrame> {
-    let file = File
-        .open(path)
-        .unwrap_or_else(|_| {
-            eprintln!("Failed to open file: {}", path);
-            process::exit(1);
-        });
+    let file = File::open(path).unwrap_or_else(|_| {
+        eprintln!("Failed to open file: {}", path);
+        process::exit(1);
+    });
     let id_password_df = BufReader::new(file)
         .lines()
         .filter_map(|line| line.ok())
@@ -60,27 +67,24 @@ fn read_id_passwords(path: &str) -> PolarsResult<DataFrame> {
             let id = split.next().unwrap();
             let password = split.next().unwrap();
             Series::new("id", vec![id.to_string()])
-                .append(Series::new("password", vec![password.to_string()]))
+                .append(&Series::new("password", vec![password.to_string()])) // Fixed: Borrow the Series
         })
         .collect::<Vec<_>>();
     DataFrame::new(id_password_df)
 }
 
 fn read_dictionary_words(path: &str) -> PolarsResult<DataFrame> {
-    let file = File
-        .open(path)
-        .unwrap_or_else(|_| {
-            eprintln!("Failed to open file: {}", path);
-            process::exit(1);
-        });
+    let file = File::open(path).unwrap_or_else(|_| {
+        eprintln!("Failed to open file: {}", path);
+        process::exit(1);
+    });
     let dict_words_df = BufReader::new(file)
         .lines()
-        .filter_map(|line| line.ok())
+        .filter_map(|line| line.ok)
         .map(|line| Series::new("words", vec![line]))
         .collect::<Vec<_>>();
     DataFrame::new(dict_words_df)
 }
-
 
 
 fn read_hashes(path: &str) -> PolarsResult<DataFrame> {
@@ -107,4 +111,28 @@ fn read_dictionary(path: &str) -> PolarsResult<DataFrame> {
         .map(|line| Series::new("words", vec![line]))
         .collect::<Vec<_>>();
     DataFrame::new(dict_df)
+}
+
+
+
+use std::collections::HashMap;
+
+fn perform_attack(dict_df: DataFrame, hash_df: DataFrame) -> HashMap<String, String> {
+    let mut results = HashMap::new();
+    let dictionary = dict_df.column("words").unwrap().utf8().unwrap();
+    let hashes = hash_df.column("hashes").unwrap().utf8().unwrap();
+
+    for word in dictionary {
+        let hash = compute_hash(word); // This function needs to be implemented based on the hash type
+        if hashes.contains(&hash) {
+            results.insert(hash, word.to_string());
+        }
+    }
+
+    results
+}
+
+fn compute_hash(word: &str) -> String {
+    // Implement the hashing algorithm here. For example, using MD5:
+    format!("{:x}", md5::compute(word))
 }
